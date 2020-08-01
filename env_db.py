449056e01867a28e7561693fabe8254a0560d5f4
Alex <alexandaryang@gmail.com>
2020-08-01 14:54:53 +0900

    add logging

--- env_db.py
+++ env_db.py
@@ -2,7 +2,7 @@
 import time
 import datetime
 import pymysql.cursors
-import logging as log
+import logging 
 import sys
 
 
@@ -12,6 +12,14 @@ class env_db():
             self.internalEnvTable = "dht11_sensor"
             self.externalEnvTable = "bme280_sensor"
 
+            self.logLevel = logging.DEBUG
+            console = logging.StreamHandler(sys.stdout)
+            formatter = logging.Formatter('%(asctime)s [%(module)-12s %(funcName)-12s] %(levelname)-8s %(message)s')
+            console.setFormatter(formatter)
+            self.log = logging.getLogger(self.__module__)
+            self.log.addHandler(console)
+            self.log.setLevel(self.logLevel)
+
             # if not exist create table
             sql_ex = ("CREATE TABLE IF NOT EXISTS {} (id int(11) DEFAULT NULL, " 
                      "probe_date datetime DEFAULT NULL,"
@@ -19,32 +27,39 @@ class env_db():
                      "hum float(4,1) DEFAULT 0,"
                      "bar float(6,1) DEFAULT 0"
                      ");").format(self.externalEnvTable)
-            print (sql_ex)
+            self.log.debug(sql_ex)
 
             sql_in = ("CREATE TABLE IF NOT EXISTS {} (id int(11) DEFAULT NULL, " 
                      "probe_date datetime DEFAULT NULL,"
                      "temp float(4,1) DEFAULT 0,"
                      "hum float(4,1) DEFAULT 0"
                      ");").format(self.internalEnvTable)
-            print (sql_in)
+            self.log.debug(sql_in)
 
-            conn = self.conn_db()
+            conn = self._conn_db()
             with self.conn.cursor() as curs:
                 curs.execute(sql_in)
                 curs.execute(sql_ex)
                 curs.close()
             conn.commit()
         except pymysql.Error as e:
-            log.error("__init__; error pymysql %d: %s" %(e.args[0], e.args[1]))
+            self.log.error("__init__; error pymysql %d: %s" %(e.args[0], e.args[1]))
             sys.exit(1)
         finally:
             if self.conn:
                 self.conn.close()
                 self.conn = None
-                log.info('__init__ closed db in init')
+                self.log.info('__init__ closed db')
+
+    def __del__(self):
+        try:
+            if self.conn:
+                self.conn.close()
+        except pymysql.Error as e:
+            print("could not add internal evn data; error pymysql %d: %s" %(e.args[0], e.args[1]))
 
 
-    def conn_db(self):
+    def _conn_db(self):
         self.conn = pymysql.connect(
                 user='alex',
                 passwd='!Passw0rd',
@@ -55,9 +70,9 @@ class env_db():
 
     def getLatestInternalData(self):
         try:
-            self.conn_db()
+            self._conn_db()
             sql = 'select * from {} limit 1;'.format(self.internalEnvTable)
-            log.debug(sql)
+            self.log.debug(sql)
 
             with self.conn.cursor() as curs:
                 curs.execute(sql)
@@ -66,34 +81,33 @@ class env_db():
                 # id, probe_date, temp, humi
                 return row[0],row[1],row[2],row[3]
         except pymysql.Error as e:
-            log.error("getLatestInternalData; error pymysql %d: %s" %(e.args[0], e.args[1]))
+            self.log.error("getLatestInternalData; error pymysql %d: %s" %(e.args[0], e.args[1]))
             sys.exit(1)
         finally:
             if self.conn:
                 self.conn.close()
                 self.conn = None
-                log.info('getLatestInternalData closed db in init')
+                self.log.info('getLatestInternalData closed db in init')
 
-    def addInternalEnvData(self, date, humidity, temperature):
+    def addInternalEnvData(self, sensor_id, date, humidity, temperature):
         try:
             if self.curs is not None:
-                sql = "insert into dht11_sensor values ({}, '{}', {}, {});".format(2, date.strftime('%Y-%m-%d %H:%M:%S'), temperature, humidity )
+                sql = "insert into dht11_sensor values ({}, '{}', {}, {});".format(sensor_id, date.strftime('%Y-%m-%d %H:%M:%S'), temperature, humidity )
                 print(sql)
-        except pymysql.Error as e:
-            print("could not add internal evn data; error pymysql %d: %s" %(e.args[0], e.args[1]))
-
-    def __del__(self):
-        try:
-            if self.conn:
-                self.conn.close()
         except pymysql.Error as e:
             print("could not add internal evn data; error pymysql %d: %s" %(e.args[0], e.args[1]))
 
 
 def main():
 
+    console = logging.StreamHandler(sys.stdout)
+    logger = logging.getLogger('env_db_test')
+    logger.addHandler(console)
+    logger.setLevel(logging.DEBUG)
+    logger.debug('start')
     db = env_db()
 
+    #logger.debug(db.__class__.__name__)
     sensor_id,probe_date,temp,humi = db.getLatestInternalData()
     print('No:', sensor_id, 'Content:', temp)
 
